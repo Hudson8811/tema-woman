91304835d1f6cabea1af776fcd20dbe147004d37
PipZip <37911683+PipZip@users.noreply.github.com>
2023-12-08 15:33:07 +0600

    update form send

--- src/js/import/main.js
+++ src/js/import/main.js
@@ -70,65 +70,102 @@ jQuery(function () {
 
 	let message = document.getElementById("js-contacts__form__textarea");
 
-	$(document).on('click', '.postcard__inter__btn', function () {
+	$(document).on('click', '.js-send-text', function () {
+	    event.preventDefault();
 	    let btn = $(this);
         let msg = $('#js-contacts__form__textarea').val();
-        if (postcardSlider instanceof Swiper) {
-            let activeSlideIndex = postcardSlider.activeIndex;
+        if (msg.trim() !== '') {
+            if (postcardSlider instanceof Swiper) {
+                let activeSlideIndex = postcardSlider.activeIndex;
+                $.ajax({
+                    url: '/save_collage/',
+                    type: 'POST',
+                    data: {
+                        postcard: activeSlideIndex,
+                        text: msg
+                    },
+                    beforeSend: function(xhr) {
+                        btn.attr('disabled',true);
+                    },
+                    success: function(response) {
+                        try {
+                            let parsedResponse = JSON.parse(response);
+                            if (parsedResponse.hasOwnProperty('link')){
+                                let imageLink = parsedResponse.link;
+                                $('.js-image-download').attr('href', imageLink);
+                                $('.js-link-input').val(imageLink);
+                                $('.postcard__inter__btns--1').hide();
+                                $('.postcard__inter__btns--2').addClass('active');
+                                $('#js-contacts__form__textarea').attr('disabled', true);
 
-            //ТЕСТОВЫЙ
-           /* $.ajax({
-                url: '/documents/text.html',
-                type: 'GET',
-                data: {
-                    postcard: activeSlideIndex,
-                    text: msg
-                },
-                beforeSend: function(xhr) {
-                    btn.attr('disabled',true);
-                },
-                success: function(response) {
-                    try {
-                        let parsedResponse = JSON.parse(response);
-                        if (parsedResponse.hasOwnProperty('link')){
-                            let imageLink = parsedResponse.link;
-                            let a = $('<a>').attr('href', imageLink).attr('download', imageLink.substring(imageLink.lastIndexOf('/') + 1));
-                            $('body').append(a);
-                            a[0].click();
-                            a.remove();
+                                postcardSlider.removeAllSlides();
+                                postcardSlider.appendSlide('<div class="postcard__slide swiper-slide js-postcard__theme"><img src="'+imageLink+'" alt=""></div>');
+                                postcardSlider.update();
+                            }
+
+                        } catch (error) {
+                            console.log('Response is not valid JSON');
                         }
-                    } catch (error) {
-                        console.log('Response is not valid JSON');
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        console.log('Error:', errorThrown);
+                    },
+                    complete: function() {
+                        btn.attr('disabled', false);
                     }
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    console.log('Error:', errorThrown);
-                },
-                complete: function() {
-                    btn.attr('disabled', false);
-                }
-            });*/
+                });
+            }
+        } else {
+            $('#js-contacts__form__textarea').focus().addClass('error').parent().addClass('error');
+        }
+	});
 
-            //БОЕВОЙ
+	$(document).on('change keyup paste','#js-contacts__form__textarea',function (){
+        $('#js-contacts__form__textarea').removeClass('error').parent().removeClass('error');
+    });
+
+
+	$(document).on('change keyup','.modal__input input',function (){
+        let value = $(this).val();
+        if (value !== '') {
+            $(this).parent().addClass('active');
+        } else {
+            $(this).parent().removeClass('active');
+        }
+    });
+
+    $(document).on('submit', '.modal__form', function () {
+        event.preventDefault();
+        let image = $(this).find('input[name=image]').val().trim();
+        let email = $(this).find('input[name=email]').val().trim();
+        let name = $(this).find('input[name=name]').val().trim();
+        if (image !== '' && email !== '' && name !== ''){
+            let btn = $(this).find('button[type=submit]');
             $.ajax({
-                url: '/save_collage/',
+                url: '/save_user/',
                 type: 'POST',
                 data: {
-                    postcard: activeSlideIndex,
-                    text: msg
+                    name: email,
+                    email: name,
+                    image: image
                 },
                 beforeSend: function(xhr) {
                     btn.attr('disabled',true);
                 },
                 success: function(response) {
                     try {
+                        let isError = false;
                         let parsedResponse = JSON.parse(response);
-                        if (parsedResponse.hasOwnProperty('link')){
-                            let imageLink = parsedResponse.link;
-                            let a = $('<a>').attr('href', imageLink).attr('download', imageLink.substring(imageLink.lastIndexOf('/') + 1));
-                            $('body').append(a);
-                            a[0].click();
-                            a.remove();
+                        if (parsedResponse.hasOwnProperty('error')){
+                            let error = parsedResponse.error;
+                            if (error !== ''){
+                                isError = true;
+                                $('.modal__error').text(error).show();
+                            }
+                        }
+                        if (!isError){
+                            $('.modal__inner').hide();
+                            $('.modal__thanks').show();
                         }
                     } catch (error) {
                         console.log('Response is not valid JSON');
@@ -141,11 +178,11 @@ jQuery(function () {
                     btn.attr('disabled', false);
                 }
             });
+        } else {
+            $('.modal__error').text('Заполнены не все поля').show();
         }
+    });
 
-
-
-	});
 	
 	
 });
